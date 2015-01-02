6a5e810d90b7521737910437f5f6de70187db255
hoohead <hoohost@googlemail.com>
2015-01-02 18:34:09 +0100

    Kommentarbereinigt und setzen globaler Variable

--- horni-bilder-grabber.php
+++ horni-bilder-grabber.php
@@ -1,66 +1,36 @@
 <?php
+//Setzen globaler Variabeln und testen
+$server = "www.hornoxe.com";
+$picdumpurl = "/hornoxe-com-picdump-395/";
 
-//echo get_content("www.hornoxe.com","/hornoxe-com-picdump-395/"); //Testen ob die Funktion greift
+$quellcode = get_content($server,$picdumpurl);
 
-//Zuerst muss die Anzahl der Seiten ermittelt werden nggpage ist Bestandteil des Links und kommt so im Quellcode nicht nochmal vor
-//Sollte dies doch mehrmals vorkommen, würde man auf den ganzen Link matchen.
 
-//Quellcode laden:
+preg_match_all('/(nggpage)(.*?)(\")/i',  $quellcode , $matches); 
 
-$quellcode = get_content("www.hornoxe.com","/hornoxe-com-picdump-395/");
-//Regex auf wie oft kommt nggpage irgendwas " (Anführungszeichen) vor
 
-preg_match_all('/(nggpage)(.*?)(\")/i',  $quellcode , $matches); //Anführungszeichen in Regex muss mit \ maskiert werden
-
-//Anzahl setzen + Sicherheitsfunktion bei Einseitigen Picdumps
 $anz_seiten = count($matches);
 if ($anz_seiten<1) {
 	$anz_seiten == 1;
 }
 
-//Testen einer validen Schleife
 for ($y=1;$y<=$anz_seiten;$y++) {
-	//echo $i."\n"; // \n = Newline
-}
-
-//Jetzt wird es spannend, da Regex bei Zeilenumbrüchen manchmal etwas suckt
-// ngg-gallery-thumbnail wurde im Quellcode eindeutig als Vorkommnis vor einem Bild erkannt
-//Wir nehmen nun den Quellcode, ersetzen den Zeilenumbruch durch ein Leerzeichen und entfernen whitespaces
-
-
-
- 
-
-//Wir haben nun ein Mehrdimmensionales Array, aus dem müssen wir nun nochmal einen Regex fahren, allerdings in einer Schleife
-
-
-for ($y=1;$y<=$anz_seiten;$y++) {
-$quellcode = get_content("www.hornoxe.com","/hornoxe-com-picdump-395/?nggpage=".$y);
-$quellcode = str_replace("\n"," ",$quellcode); //wir können ruhig in eine verwendete Anfrage direkt reinschreiben, PHP ist da recht schmerzfrei
+$quellcode = get_content($server,$picdumpurl."?nggpage=".$y);
+$quellcode = str_replace("\n"," ",$quellcode); 
 $quellcode = trim($quellcode);
 preg_match_all('/(ngg-gallery-thumbnail\")(.*?)(title)/i',  $quellcode , $matches2);
 $anz_bilder = count($matches2[2]);
 	
-	for ($i=0;$i<$anz_bilder;$i++) { //unser Array fängt bei 0 an, deshalb müssen wir 1 kleiner als Anzahl gehen
+	for ($i=0;$i<$anz_bilder;$i++) { 
 		preg_match('/(href=\")(.*?)(\")/i',  $matches2[2][$i], $matches3);
-		$bildurl = $matches3[2];   //wenn wir gezielt cutten, ist [2] die richtige Wahl
+		$bildurl = $matches3[2];   
 		$bildname = basename($matches3[2]);
-		$bild = file_get_contents($bildurl); // da hier nicht mit Schutzmechanismen zu rechnen ist, setze ich hier file_get_contents ein
-		//aber auch hier könnte man Dinge wie Referer oder Cookies mit übergeben ...
-		//da Bilder recht klein sind, ist das temporäre zwischenspeichern in einer Variable unkritisch
-		file_put_contents($bildname,$bild); //speichern des Bildes auf der Festplatte
+		$bild = file_get_contents($bildurl); 
+		file_put_contents($bildname,$bild); 
 		 
 	}
 }
 
-// Es steht fest, dass es in dem Beispiel 4 Seiten sind, die werden auch durch den count so ermittelt
-// Manuell wurde überprüft, ob hinter ?nggpage=1 auch die 1. Seite aufgerufen wird.
-// Da dies so ist, kann man sich die 4 Seiten, bzw. die ermittelte Anzahl von Seiten laden, ohne einen Fehler zu provozieren
-
-//Weiter geht es
-
-
-//Packen des Socketscriptes in eine Funktion, da wir darauf mehrmals zugreifen müssen
 function get_content($host,$pfad) {
 
 $sock = fsockopen($host, 80); 
