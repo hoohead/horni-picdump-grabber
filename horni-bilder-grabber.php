4da228c50b86ebefed9d076dbfd2183a4d8c16fb
hoohead <hoohost@googlemail.com>
2015-01-02 17:09:47 +0100

    Packen des Grabbers in eine Funktion

--- horni-bilder-grabber.php
+++ horni-bilder-grabber.php
@@ -1,9 +1,13 @@
 <?php
 
 
-$sock = fsockopen("www.hornoxe.com", 80); 
-fputs($sock, "GET /hornoxe-com-picdump-395/ HTTP/1.1\r\n");
-fputs($sock, "Host: www.ghornoxe.com\r\n");
+
+//Packen des Socketscriptes in eine Funktion, da wir darauf mehrmals zugreifen müssen
+function get_content($host,$pfad) {
+
+$sock = fsockopen($host, 80); 
+fputs($sock, "GET ".$url." HTTP/1.1\r\n");
+fputs($sock, "Host: ".$pfad."\r\n");
 fputs($sock, "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0\r\n");
 fputs($sock, "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n");
 fputs($sock, "Accept-Language: de,de-DE;q=0.5\r\n");
@@ -13,7 +17,7 @@ while(!feof($sock))
 $bla .= fgets($sock, 4096); 
 fclose($sock);
 
-echo $bla;
+return $bla;
 
-
+}
 ?>
