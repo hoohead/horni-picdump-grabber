2db9fa63b33b207e1e7e9613979988854b5a0059
hoohead <hoohost@googlemail.com>
2015-01-02 16:54:35 +0100

    Socketscript copy-pasten

---
+++ horni-bilder-grabber.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$sock = fsockopen($host, 80); 
+fputs($sock, "GET / HTTP/1.1\r\n");
+fputs($sock, "Host: www.google.de\r\n");
+fputs($sock, "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0\r\n");
+fputs($sock, "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n");
+fputs($sock, "Accept-Language: de,de-DE;q=0.5\r\n");
+fputs($sock, "Connection: close\r\n\r\n");
+
+while(!feof($sock))
+$bla2 .= fgets($sock, 4096); 
+fclose($sock);
+
+echo $bla;
+
+
+?>
